ab669038a4208baad9cf32c9a9c1710738a27dd5
aadarshdixit <adarshdixit2004@gmail.com>
2024-12-14 15:40:00 +0530

    test

--- src/server.cpp
+++ src/server.cpp
@@ -53,10 +53,17 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  if(client_fd<0){
+    close(client_fd);
+    close(server_fd);
+    std::cout<<" Client connection failed/n";
+    return 1;
+  }
   std::cout << "Client connected\n";
-  
+  std::string response = "HTTP/1.1 200 OK\r\n\r\n";
+  int data = write(client_fd, response.c_str(),sizeof(response));
+  close(client_fd);
   close(server_fd);
-
   return 0;
 }
