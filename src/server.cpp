57c0187ca3513431cda89e1be2c413a2391fdb7b
aadarshdixit <adarshdixit2004@gmail.com>
2024-12-14 21:11:36 +0530

    test

--- src/server.cpp
+++ src/server.cpp
@@ -4,13 +4,23 @@
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
+// #include <netdb.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 using namespace std;
 
+#pragma comment(lib, "ws2_32.lib")
+
 int main(int argc, char **argv) {
+
+  WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock." << std::endl;
+        return 1;
+    }
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -29,7 +39,7 @@ int main(int argc, char **argv) {
   // // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
@@ -66,20 +76,24 @@ int main(int argc, char **argv) {
   std::string response = "HTTP/1.1 404 Not Found\r\n\r\n";
 
   char buf[1024];
-	int rc = recv(client_fd, &buf, sizeof(buf), 0);
+	int rc = recv(client_fd, buf, sizeof(buf), 0);
   int i=0;
   while(buf[i]!=' ') i++;
   i++;
   string url_path;
   while(buf[i]!=' ') {
     url_path+=buf[i];
+    i++;
   }
-  cout<<url_path<<endl;
+
+
   if(url_path == "/") response = "HTTP/1.1 200 OK\r\n\r\n";
 
   int data = write(client_fd, response.c_str(),sizeof(response));
 
   close(client_fd);
   close(server_fd);
+
+    WSACleanup(); 
   return 0;
 }
