250b3794b0a88c4427d44ea1356cb8005346b9a6
aadarshdixit <adarshdixit2004@gmail.com>
2024-12-15 14:38:06 +0530

    test

--- src/server.cpp
+++ src/server.cpp
@@ -4,11 +4,11 @@
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <arpa/inet.h>
-// #include <netdb.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+// #include <winsock2.h>
+// #include <ws2tcpip.h>
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -34,14 +34,14 @@ string get_header_data(string header){
   vector<string> temp2 = split_message(header," ");
   return temp2[1];
 }
-#pragma comment(lib, "Ws2_32.lib")
+// #pragma comment(lib, "Ws2_32.lib")
 int main(int argc, char **argv) {
 
-  WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Failed to initialize Winsock." << std::endl;
-        return 1;
-    }
+  // WSADATA wsaData;
+  //   if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+  //       std::cerr << "Failed to initialize Winsock." << std::endl;
+  //       return 1;
+  //   }
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -128,6 +128,6 @@ int main(int argc, char **argv) {
   close(client_fd);
   close(server_fd);
 
-    WSACleanup(); 
+    // WSACleanup(); 
   return 0;
 }
